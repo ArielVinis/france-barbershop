10c189e88ec50bc7b5230a0e9e878a6892c91959
Ariel França <ariel.franca@hotmail.com>
2026-01-10 21:50:18 -0300

    fix: update process.env usage to env import for consistency

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 import { Pool } from "pg"
+import { env } from "process"
 
 declare global {
   // eslint-disable-next-line no-unused-vars
@@ -8,13 +9,13 @@ declare global {
 }
 
 let prisma: PrismaClient
-if (process.env.NODE_ENV === "production") {
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+if (env.NODE_ENV === "production") {
+  const pool = new Pool({ connectionString: env.DATABASE_URL })
   const adapter = new PrismaPg(pool)
   prisma = new PrismaClient({ adapter })
 } else {
   if (!global.cachedPrisma) {
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+    const pool = new Pool({ connectionString: env.DATABASE_URL })
     const adapter = new PrismaPg(pool)
     global.cachedPrisma = new PrismaClient({ adapter })
   }
