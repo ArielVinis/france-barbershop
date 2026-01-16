925d11c2a2c3cf343dc6ea98a654e2f9322ec815
Ariel França <ariel.franca@hotmail.com>
2026-01-16 02:39:46 -0300

    feat: enhance bookings page with user session check and updated messaging

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,29 +1,33 @@
-import { getServerSession } from "next-auth"
 import Header from "../_components/header"
-import { authOptions } from "../_lib/auth"
-import { notFound } from "next/navigation"
 import BookingItem from "../_components/booking-item"
 import { getConfirmedBookings } from "../_data/get-confirmed-bookings"
 import { getConcludedBookings } from "../_data/get-concluded-bookings"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
+import AuthRequiredDialog from "../_components/auth-required-dialog"
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions)
-  if (!session?.user) {
-    // TODO: mostrar pop-up de login
-    return notFound()
-  }
   const confirmedBookings = await getConfirmedBookings()
   const concludedBookings = await getConcludedBookings()
 
   return (
     <>
       <Header />
+      <AuthRequiredDialog />
       <div className="space-y-3 p-5">
         <h1 className="text-xl font-bold">Agendamentos</h1>
-        {confirmedBookings.length === 0 && concludedBookings.length === 0 && (
-          <p className="text-gray-400">Você não possui agendamentos.</p>
+        {!session?.user && (
+          <p className="text-gray-400">
+            Faça login para ver seus agendamentos.
+          </p>
         )}
-        {confirmedBookings.length > 0 && (
+        {session?.user &&
+          confirmedBookings.length === 0 &&
+          concludedBookings.length === 0 && (
+            <p className="text-gray-400">Você não possui agendamentos.</p>
+          )}
+        {session?.user && confirmedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
               Confirmados
@@ -36,7 +40,7 @@ const Bookings = async () => {
             ))}
           </>
         )}
-        {concludedBookings.length > 0 && (
+        {session?.user && concludedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
               Finalizados
