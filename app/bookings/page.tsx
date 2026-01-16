43d4f05f681b3afcbc19ec7daf719b8f8dd3483f
Ariel França <ariel.franca@hotmail.com>
2026-01-16 09:55:10 -0300

    feat: create BookingsClient component

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,10 +1,9 @@
+import { getServerSession } from "next-auth"
 import Header from "../_components/header"
-import BookingItem from "../_components/booking-item"
+import { authOptions } from "../_lib/auth"
 import { getConfirmedBookings } from "../_data/get-confirmed-bookings"
 import { getConcludedBookings } from "../_data/get-concluded-bookings"
-import { getServerSession } from "next-auth"
-import { authOptions } from "../_lib/auth"
-import AuthRequiredDialog from "../_components/auth-required-dialog"
+import BookingsClient from "./bookings-client"
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions)
@@ -14,46 +13,11 @@ const Bookings = async () => {
   return (
     <>
       <Header />
-      <AuthRequiredDialog />
-      <div className="space-y-3 p-5">
-        <h1 className="text-xl font-bold">Agendamentos</h1>
-        {!session?.user && (
-          <p className="text-gray-400">
-            Faça login para ver seus agendamentos.
-          </p>
-        )}
-        {session?.user &&
-          confirmedBookings.length === 0 &&
-          concludedBookings.length === 0 && (
-            <p className="text-gray-400">Você não possui agendamentos.</p>
-          )}
-        {session?.user && confirmedBookings.length > 0 && (
-          <>
-            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-              Confirmados
-            </h2>
-            {confirmedBookings.map((booking) => (
-              <BookingItem
-                key={booking.id}
-                booking={JSON.parse(JSON.stringify(booking))}
-              />
-            ))}
-          </>
-        )}
-        {session?.user && concludedBookings.length > 0 && (
-          <>
-            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-              Finalizados
-            </h2>
-            {concludedBookings.map((booking) => (
-              <BookingItem
-                key={booking.id}
-                booking={JSON.parse(JSON.stringify(booking))}
-              />
-            ))}
-          </>
-        )}
-      </div>
+      <BookingsClient
+        session={session}
+        confirmedBookings={confirmedBookings}
+        concludedBookings={concludedBookings}
+      />
     </>
   )
 }
