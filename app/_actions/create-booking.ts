8fdfeb5d55ddebf65298f11eefa55dd9b2c7748b
Ariel França <ariel.franca@hotmail.com>
2026-01-16 09:18:29 -0300

    feat: update createBooking function to use session for user authentication

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -7,16 +7,17 @@ import { authOptions } from "../_lib/auth"
 
 interface CreateBookingParams {
   serviceId: string
+  barberId?: string
   date: Date
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
-  const user = await getServerSession(authOptions)
-  if (!user) {
+  const session = await getServerSession(authOptions)
+  if (!session) {
     throw new Error("Usuário não autenticado")
   }
   await db.booking.create({
-    data: { ...params, userId: (user.user as any).id },
+    data: { ...params, userId: (session.user as any).id },
   })
   revalidatePath("/barbershops/[id]")
   revalidatePath("/bookings")
