5a86fa8a0d4bc227dda7d0ee36a1db77242296e6
France Dev <france.dev@outlook.com>
2026-01-29 00:43:08 -0300

    chore: migrate ESLint configuration to eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "eslint/config";
+import nextConfig from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextConfig,
+  {
+    rules: {
+      "no-unused-vars": "error",
+    },
+  },
+]);
