300581633735fad8f98e64ad9ba5a8eca32e55b4
France Dev <france.dev@outlook.com>
2026-01-29 22:39:18 -0300

    feat: add PATHS constants

---
+++ app/_constants/PATHS.ts
@@ -0,0 +1,17 @@
+export const PATHS = Object.freeze({
+  HOME: "/",
+  BARBER: {
+    HOME: "/barber",
+    BOOKINGS: "/barber/bookings",
+    SETTINGS: "/barber/settings",
+    PROFILE: "/barber/profile",
+    RATINGS: "/barber/ratings",
+  },
+  BARBERSHOP: {
+    HOME: "/barbershops",
+    DETAIL: "/barbershops/:slug",
+  },
+  BOOKINGS: {
+    HOME: "/bookings",
+  },
+})
