0fcfa44d8ce4b8f5968aeebd13b7f7f5529be488
France Dev <france.dev@outlook.com>
2026-04-11 14:46:01 -0300

    fix: update navigation paths to use PANEL instead

--- src/app/(stripe)/payment-confirmation/page.tsx
+++ src/app/(stripe)/payment-confirmation/page.tsx
@@ -30,7 +30,7 @@ export default async function Return({ searchParams }: ReturnProps) {
   const customerEmail = session.customer_details?.email ?? null
 
   if (status === "open") {
-    return redirect(PATHS.OWNER.HOME)
+    return redirect(PATHS.PANEL.ROOT)
   }
 
   if (status === "expired") {
@@ -38,7 +38,7 @@ export default async function Return({ searchParams }: ReturnProps) {
       <section id="expired">
         <p>A sessão de pagamento expirou. Por favor, tente novamente.</p>
         <Button asChild>
-          <Link href={PATHS.HOME}>Voltar para a página de checkout</Link>
+          <Link href={PATHS.ROOT}>Voltar para a página de checkout</Link>
         </Button>
       </section>
     )
@@ -67,7 +67,7 @@ export default async function Return({ searchParams }: ReturnProps) {
             </div>
             <div className="pt-4">
               <Button variant="outline" asChild>
-                <Link href={PATHS.OWNER.HOME}>
+                <Link href={PATHS.PANEL.ROOT}>
                   <HomeIcon />
                   Acessar o painel do proprietário
                 </Link>
