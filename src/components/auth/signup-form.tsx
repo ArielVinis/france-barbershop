fa6883604e5000f303e0f604362b7ca371f7d49c
France Dev <france.dev@outlook.com>
2026-04-26 12:07:31 -0300

    feat: enhance login and signup forms with improved UI components and Google sign-in functionality; update labels and error messages to Portuguese

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -3,24 +3,35 @@
 import { cn } from "@/src/lib/utils"
 import { Button } from "@/src/components/ui/button"
 import { Input } from "@/src/components/ui/input"
-import { Label } from "@/src/components/ui/label"
 import Link from "next/link"
 import { PATHS } from "@/src/constants/PATHS"
 import { signUp } from "@/src/server/auth/users"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import z from "zod"
-import { Form, FormField } from "../ui/form"
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormControl,
+  FormMessage,
+  FormLabel,
+} from "../ui/form"
 import { toast } from "sonner"
 import { useTransition } from "react"
 import { Loader2 } from "lucide-react"
 import { useRouter } from "next/navigation"
+import { authClient } from "@/src/lib/auth-client"
+import Image from "next/image"
 
 const formSchema = z.object({
-  name: z.string().min(1),
-  email: z.string().email(),
-  password: z.string().min(8),
-  confirmPassword: z.string().min(8),
+  name: z.string().min(3, "Nome deve ter pelo menos 3 caracteres").trim(),
+  email: z.string().email("Email inválido").trim(),
+  password: z.string().min(8, "Senha deve ter pelo menos 8 caracteres").trim(),
+  confirmPassword: z
+    .string()
+    .min(8, "Senha deve ter pelo menos 8 caracteres")
+    .trim(),
 })
 
 export function SignupForm({
@@ -57,6 +68,13 @@ export function SignupForm({
     })
   }
 
+  const signUpWithGoogle = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: PATHS.PANEL.ROOT,
+    })
+  }
+
   return (
     <Form {...form}>
       <form
@@ -65,25 +83,30 @@ export function SignupForm({
         {...props}
       >
         <div className="flex flex-col items-center gap-2 text-center">
-          <h1 className="text-2xl font-bold">Create your account</h1>
+          <h1 className="text-2xl font-bold"> Crie sua conta</h1>
           <p className="text-balance text-sm text-muted-foreground">
-            Fill in the form below to create your account
+            Preencha o formulário abaixo para criar sua conta
           </p>
         </div>
         <div className="grid gap-6">
           <div className="grid gap-2">
-            <Label htmlFor="name">Full Name</Label>
             <FormField
               control={form.control}
               name="name"
               render={({ field }) => (
-                <Input
-                  id="name"
-                  type="text"
-                  placeholder="John Doe"
-                  required
-                  {...field}
-                />
+                <FormItem>
+                  <FormLabel>Nome Completo</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="name"
+                      type="text"
+                      placeholder="João da Silva"
+                      required
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
             />
           </div>
@@ -93,73 +116,96 @@ export function SignupForm({
             control={form.control}
             name="email"
             render={({ field }) => (
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-                {...field}
-              />
+              <FormItem>
+                <FormLabel>Endereço de e-mail</FormLabel>
+                <FormControl>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="joao@example.com"
+                    required
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
             )}
           />
         </div>
         <p className="text-sm text-muted-foreground">
-          We&apos;ll use this to contact you. We will not share your email with
-          anyone else.
+          Não compartilharemos seu e-mail com ninguém.
         </p>
         <div className="grid gap-2">
           <FormField
             control={form.control}
             name="password"
             render={({ field }) => (
-              <Input id="password" type="password" required {...field} />
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="A senha deve ter 8 caracteres"
+                    required
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
             )}
           />
-          <p className="text-sm text-muted-foreground">
-            Must be at least 8 characters long.
-          </p>
         </div>
         <div className="grid gap-2">
           <FormField
             control={form.control}
             name="confirmPassword"
             render={({ field }) => (
-              <Input id="confirmPassword" type="password" required {...field} />
+              <FormItem>
+                <FormLabel>Confirmar Senha</FormLabel>
+                <FormControl>
+                  <Input
+                    id="confirmPassword"
+                    type="password"
+                    placeholder="Confirme a senha"
+                    required
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
             )}
           />
-          <p className="text-sm text-muted-foreground">
-            Please confirm your password.
-          </p>
         </div>
         <div className="grid gap-2">
           <Button type="submit" disabled={isPending}>
             {isPending ? (
               <Loader2 className="size-4 animate-spin" />
             ) : (
-              "Create Account"
+              "Criar Conta"
             )}
           </Button>
         </div>
         <div className="grid gap-2">
-          <p className="text-sm text-muted-foreground">Or continue with</p>
+          <p className="text-sm text-muted-foreground">Ou continue com</p>
         </div>
         <div className="grid gap-2">
-          <Button variant="outline" type="button">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path
-                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-                fill="currentColor"
-              />
-            </svg>
-            Sign up with GitHub
+          <Button variant="outline" type="button" onClick={signUpWithGoogle}>
+            <Image
+              alt="Fazer login com o Google"
+              src="/google.svg"
+              width={18}
+              height={18}
+            />
+            Criar conta com Google
           </Button>
           <p className="text-center text-sm">
-            Already have an account?{" "}
+            Já tem uma conta?{" "}
             <Link
               href={PATHS.AUTH.LOGIN}
               className="underline underline-offset-4"
             >
-              Sign in
+              Fazer login
             </Link>
           </p>
         </div>
