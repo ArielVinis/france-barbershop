614314eee7b24973120076086448b4dc936a7c26
France Dev <france.dev@outlook.com>
2026-05-01 18:45:31 -0300

    refactor: update proxy logic to use getCurrentUser for role-based redirection and adjust path matching for panel and dev routes

--- src/app/proxy.ts
+++ src/app/proxy.ts
@@ -1,31 +1,22 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
-import { auth } from "@/src/lib/auth"
 import { PATHS } from "@/src/constants/PATHS"
-import { headers } from "next/headers"
+import { getCurrentUser } from "../server/auth/users"
 
 export async function proxy(req: NextRequest) {
-  // const { pathname } = req.nextUrl
+  const { pathname } = req.nextUrl
 
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
-
+  const { user } = await getCurrentUser()
   // Ajustar para pegar o role do user
-  // if (pathname.startsWith(PATHS.PANEL.ROOT)) {
-  //   const notAuthorized = new URL(PATHS.NOT_AUTHORIZED, req.url)
-  //   if (session?.user.role !== "OWNER" || "BARBER") {
-  //     return NextResponse.redirect(notAuthorized)
-  //   }
-  // }
-
-  if (!session) {
-    return NextResponse.redirect(new URL(PATHS.AUTH.LOGIN, req.url))
+  if (pathname.startsWith(PATHS.PANEL.ROOT || PATHS.DEV.PANEL)) {
+    if (user.role !== "OWNER" || "BARBER") {
+      return NextResponse.redirect(PATHS.NOT_AUTHORIZED)
+    }
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/panel/:path*"],
+  matcher: ["/panel/:path*", "/dev/:path*"],
 }
