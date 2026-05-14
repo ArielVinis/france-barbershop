0143ac50594d65aa38043924e40eda299bb1d2dd
France Dev <france.dev@outlook.com>
2026-05-14 01:09:10 -0300

    refactor: reorganize exports in authz module and remove unused resolvePanelContext function to streamline codebase

--- src/lib/authz/index.ts
+++ src/lib/authz/index.ts
@@ -1,11 +1,7 @@
-export { getBarbershopForUser, getBarbershopsForUser } from "./get-barbershops-for-user"
+export {
+  getBarbershopForUser,
+  getBarbershopsForUser,
+} from "./get-barbershops-for-user"
 export { getBarberForUser } from "./get-barber-for-user"
 export { requireBarbershopForOwner } from "./require-barbershop-for-owner"
-export { resolvePanelContext } from "./resolve-panel-context"
 export { ForbiddenError, NotFoundError, ValidationError } from "./errors"
-export { isOwnerContext } from "@/src/types/panel-context"
-export type {
-  PanelContext,
-  PanelContextBarber,
-  PanelContextOwner,
-} from "@/src/types/panel-context"
