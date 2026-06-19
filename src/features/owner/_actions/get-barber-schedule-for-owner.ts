1dffe3dc8e1dbc4bfee608139d88cc291c6030fb
France Dev <france.dev@outlook.com>
2026-06-18 23:15:08 -0300

    chore: remove unused owner-related action and data files to streamline codebase and improve organization

--- src/features/owner/_actions/get-barber-schedule-for-owner.ts
+++
@@ -1,9 +0,0 @@
-"use server"
-
-import { getCurrentUser } from "@/src/server/auth/users"
-import { getBarberForOwner } from "@/src/features/owner/_data/get-barber-for-owner"
-
-export async function getBarberScheduleForOwner(barberId: string) {
-  const { user } = await getCurrentUser()
-  return getBarberForOwner(barberId, user.id)
-}
