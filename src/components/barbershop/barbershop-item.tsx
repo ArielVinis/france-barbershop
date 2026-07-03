74bc60d4062162b509d5c5c6510fb77f49bb31b7
France Dev <france.dev@outlook.com>
2026-07-02 22:59:18 -0300

    fix: update button text for barbershop item link
    - Changed the button text from "Reservar" to "Acessar" for improved clarity in navigation.

--- src/components/barbershop/barbershop-item.tsx
+++ src/components/barbershop/barbershop-item.tsx
@@ -50,7 +50,7 @@ const BarbershopItem = ({
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
           <Button variant="secondary" className="mt-3 w-full" asChild>
-            <Link href={PATHS.BARBERSHOP.ROOT(barbershop.slug)}>Reservar</Link>
+            <Link href={PATHS.BARBERSHOP.ROOT(barbershop.slug)}>Acessar</Link>
           </Button>
         </div>
       </CardContent>
